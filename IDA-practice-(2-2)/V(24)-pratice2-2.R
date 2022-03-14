646683150c00732d307e3d4695bf19da09c1e85c
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2022-03-14 15:08:48 +0300

    IDA-practice-(2-2) created

---
+++ IDA-practice-(2-2)/V(24)-pratice2-2.R
@@ -0,0 +1,29 @@
+library("lmtest")
+library("GGally")
+library("car")
+
+data = na.omit(attitude) # insertion database exclude NA answers
+      # rating- объ€сн€ема€ переменна€
+      # {raises, critical, advance}- регрессоры
+
+help(attitude)
+
+model_1 = lm(rating ~ I(log(raises)) + I(log(advance)), data)
+summary(model_1)
+
+t_value = qt(0.975, df = 27)
+t_value
+
+
+new.data = data.frame(raises = 34, advance = 51)
+
+predict(model_1, new.data, interval = "confidence")
+
+
+
+
+
+
+
+
+
