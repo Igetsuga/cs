3ad23664958add84e2aaa62315544c99dfe8cd4a
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2022-05-22 14:14:03 +0300

    in process

--- object-oriented-programming/practice6[list,stack].cpp
+++ object-oriented-programming/practice6[list,stack].cpp
@@ -1,87 +1,145 @@
-#include <iomanip>
+п»ї#include <iomanip>
 #include <iostream>
 #include <fstream>
+// #include "LinkedList.cpp"
 
-// Abstract class. 
-class Information {};
+///////////////////////////////////////////////////////////////////////////////
+// class Human. 
+class Human
+{
+public:
+    // default constructor
+    Human(std::string name = "unknown", std::string surname = "unknown")
+    {
+        name_ = name;
+        surname_ = surname;
+    }
+
+    // copy constructor.
+    Human(const Human &other) = default;
+
+    // operator= .
+    Human& operator= (const Human &other) = default;
+
+    // destructor.
+    virtual ~Human() { std::cout << "Object \"Human\" was deleted" << '\n'; } // = default;
 
 
 
-// class InformationProgrammer. Contain all inforamtion about object `Object<InformatrionProgrammer>`.
-class InformationProgrammer : public Information
+    // Get & Set.
+    std::string GetName() { return name_; }
+    std::string GetSurname() { return surname_; }
+    void SetName(std::string name) { name_ = name; }
+    void SetSurname(std::string surname) { surname_ = surname; }
+
+
+
+protected:
+    std::string name_;
+    std::string surname_;
+};
+
+
+
+// class Worker.
+class Worker : public Human
+{
+public:
+    // default constructor
+    Worker(std::string job = "unknown") { job_ = job; }
+
+    // copy constructor.
+    Worker(const Worker &other) = default;
+
+    // operator= .
+    Worker& operator= (const Worker &other) = default;
+
+    // destructor.
+    virtual ~Worker() { std::cout << "Object \"Worker\" was deleted" << '\n'; }// = default;
+
+
+
+    // Get & Set.
+    std::string GetJob() { return job_; }
+    void SetJob(std::string job) { job_ = job; }
+
+
+
+protected:
+    std::string job_;
+};
+
+
+
+class Programmer : public Worker
 {
 public:
     // default constructor.
-    InformationProgrammer (const int         &level =              0,
-                           const std::string &lang =       "unknown",
-                           const std::string &name =       "unknown",
-                           const std::string &surname =    "unknown",
-                           const std::string &email =      "unknown",
-                           const std::string &telegramID = "unknown",
-                           const std::string &skypeID =    "unknown",
-                           const std::string &job =        "unknown")
+    Programmer (const int &level = 0,
+        const std::string &lang       = "unknown",
+        const std::string &name       = "unknown",
+        const std::string &surname    = "unknown",
+        const std::string &email      = "unknown",
+        const std::string &telegramID = "unknown",
+        const std::string &skypeID    = "unknown",
+        const std::string &job        = "unknown")
 
     {
-        level_      = level;
-        lang_       = lang;
-        name_       = name;
-        surname_    = surname;
-        email_      = email;
-        telegramID_ = telegramID;
-        skypeID_    = skypeID;
-        job_        = job;
+        Programmer::level_      = level;
+        Programmer::lang_       = lang;
+        Human::name_            = name;
+        Human::surname_         = surname;
+        Programmer::email_      = email;
+        Programmer::telegramID_ = telegramID;
+        Programmer::skypeID_    = skypeID;
+        Worker::job_            = job;
     }
-
-
-
-    // destructor
-    ~InformationProgrammer() { std::cout << "Object \"InformationPragrammer\" was deleted" << '\n'; }
-
-
 
     // copy constructor.
-    InformationProgrammer (const InformationProgrammer &programmer)
+    Programmer(const Programmer &other)
     {
-        level_ = programmer.level_;
-        lang_ = programmer.lang_;
-        name_ = programmer.name_;
-        surname_ = programmer.surname_;
-        email_ = programmer.email_;
-        telegramID_ = programmer.telegramID_;
-        skypeID_ = programmer.skypeID_;
-        job_ = programmer.job_;
+        // this->Human = other.Human
+        this->Human::name_ = other.Human::name_;
+        this->Human::surname_ = other.Human::surname_;
+
+        this->Worker::job_ = other.Worker::job_;
+
+        this->Programmer::level_      = other.Programmer::level_;
+        this->Programmer::lang_       = other.Programmer::lang_;
+        this->Programmer::email_      = other.Programmer::email_;
+        this->Programmer::telegramID_ = other.Programmer::telegramID_;
+        this->Programmer::skypeID_    = other.Programmer::skypeID_;
     }
 
+    // destructor
+    ~Programmer() { std::cout << "Object \"Pragrammer\" was deleted" << '\n'; }
+ 
 
 
-    friend std::ostream& operator<< (std::ostream &output, const InformationProgrammer &programmer);
+    friend std::ostream& operator<< (std::ostream &output, const Programmer &other);
 
     void SetLevel(int level) { level_ = level; }
-
 protected:
     int            level_;
     std::string    lang_;
-    std::string    name_;
-    std::string    surname_;
     std::string    email_;
     std::string    telegramID_;
     std::string    skypeID_;
-    std::string    job_;
 };
-std::ostream& operator<< (std::ostream &output, const InformationProgrammer &programmer)
+std::ostream& operator<< (std::ostream &output, const Programmer &other)
 {
     output << '\n' << "---------------------START--------------------" << '\n';
 
-    output << "   name: " << programmer.name_ << '\n'
-           << "surname: " << programmer.surname_ << '\n'
+    output << "   name: " << other.Human::name_ << '\n'
+           << "surname: " << other.Human::surname_ << '\n'
                    <<"------------------Information:----------------" << '\n';
 
-    output << "     level: " << programmer.level_ << '\n'
-           << "      lang: " << programmer.lang_ << '\n'
-           << "     email: " << programmer.email_ << '\n'
-           << "telegramID: " << programmer.telegramID_ << '\n'
-           << "   skypeID: " << programmer.skypeID_ << '\n'
-           << "       job: " << programmer.job_ << '\n' 
+    output << "     level: " << other.Programmer::level_ << '\n'
+           << "      lang: " << other.Programmer::lang_ << '\n'
+           << "     email: " << other.Programmer::email_ << '\n'
+           << "telegramID: " << other.Programmer::telegramID_ << '\n'
+           << "   skypeID: " << other.Programmer::skypeID_ << '\n'
+           << "       job: " << other.Worker::job_ << '\n'
                    << "----------------------END---------------------" << '\n';
 
 
@@ -90,133 +148,124 @@ std::ostream& operator<< (std::ostream &output, const InformationProgrammer &pro
 
 
 
-// template of `class Object`.
-template<class Type> class Object
+
+
+///////////////////////////////////////////////////////////////////////////////
+// LinkedList.
+template<class Type> class LinkedList
 {
+    class Object
+    {
+    public:
+        // default constructor.
+        Object (const Type &data, Object *sucessor = nullptr)
+        {
+            sucessor_ = sucessor;
+            data_ = data;
+        }
+
+        // destructor.
+        ~Object() = default;
+    protected:
+        Object *sucessor_;
+        Type    data_;
+    };
 public:
-    // default constructor
-    Object<Type>(const Type &information)
+    // default constructor.
+    LinkedList()
     {
-        predecessor_ = sucessor_ = nullptr;
-        information_ = information;
+        head_ = nullptr;
+        size_ = 0;
     }
-
-
-
-    // full constructor
-    Object<Type>(const Type& information, Object<Type> *predecessor = nullptr, Object<Type> *sucessor = nullptr)
-    // Object<Type>(const Object<Type>* prev, cosnt Object<Type> *next, const Information<Type> &info)
-        // почему не работает с константными указателями?
-    {
-        predecessor_ = predecessor;
-        sucessor_ = sucessor;
-        information_ = information;
-    }
-
-
-
-    // copy constructor
-    Object<Type>(const Object<Type> &object)
-    {
-        predecessor_ = new Object<Type>(*object.predecessor_);
-        sucessor_ = object.sucessor_;
-        information_ = object.information_;
-    }
-
-
-
-    // get & set 
-    Object* GetPredecessor() { return predecessor_; } const 
-    void SetPredecessor(Object* predecessor) { predecessor_ = predecessor; }
-
-    Object* GetSucessor() { return sucessor_; } const
-    void SetSucessor(Object* sucessor) { sucessor_ = sucessor; }
-
-
-
-    friend std::ostream& operator<< (std::ostream &output, const Object<Type> &object);
-
-
-
-protected:
-    Object  *sucessor_;
-    Object  *predecessor_; 
-    Type   information_;
-};
-template<class Type> std::ostream& operator<< (std::ostream &output, const Object<Type> &object)
-{
-    if (typeid(output).name() == typeid(std::ostream).name())
-    {
-
-    }
-    else
-    {
-        output << '\n' << typeid(object).name() << '\n';
-
-        output << "Previous value is: " << *(object.predecessor_) 
-            << '\n' << "Next value is: " << *(object.sucessor_) << object.information_;
-    }
-
-
-    return output;
-}
-
-
-// Linked list.
-template<class Type> class List
-{
-    // defaul constructor.
-    List<Type>()
-    {
-        head_ = tail_ = nullptr;
-        amount_ = 0;
-    }
-
-
 
     // copy constructor.
-    //List<Type> (const List<Type> &list)
-    //{
-    //    if (list.head_ == nullptr) // <=> if (list.amount_ == 0) 
-    //    {
-    //        head_ = tail_ = nullptr;
-    //        amount_ = 0;
-    //    }
-    //    else
-    //    {
-    //        for (Object<Type>* ptr = list.head_; ptr != nullptr; ptr = ptr->sucessor)
-    //        {
-    //            Object<Type> object = new Object<Type>(ptr->information_);
+    LinkedList (const LinkedList &other)
+    {
+        if (other.size_ != 0)
+        {
+            head_ = new Object(other.head_->data_); // (*head_).data_ = (*other.head).data_;
+                                                    // (*head_).sucessor_ = nullptr;
+            Object *ptr_current_this = head_;
+            Object *ptr_current_other = other.head_;
 
-    //            if (head_ == nullptr) 
-    //            {
-    //                head->predes
-    //                head_ = object;
-    //            }
+            while (ptr_current_other != nullptr)
+            {
+                // (*ptr_current_this).data_ = (*ptr_current_other).data_; 
+                // (*ptr_current_this).sucessor_ = nullptr;
+                ptr_current_this->sucessor_ = new Object(ptr_current_other->sucessor_->data_);
 
+                ptr_current_this = ptr_current_this->sucessor_; // ptr_current_this = nullptr;
+                ptr_current_other = ptr_current_other->sucessor_;
+            }
+        }
+        else 
+        {
+            head_ = nullptr;
+        }
 
-    //        }
-    //    }
-    //    
-    //}
+        size_ = other.size_;
+    }
 
+    // LinkedList::list1 = LinkedList::list2 ==> list1.
+    // HELP
+    LinkedList& operator= (const LinkedList &other) const // coping, not rename other.
+                                                          // this->sucessor_ != other->sucessor_.
+                                                          // this->data_ = other->data_.
+    {
+        this->clear();
 
+        size_ = other.size_;
+        head_ = nullptr;
 
-    //~List<Type>()
-    //{
-    //    if (list.head_ != nullptr) // <=> if (list.amount_ != 0) 
-    //    {
-    //        for (Object<Type>* ptr1 = ptr2 = head_, ; ptr1 != tail; ptr1 = ptr2;) // ptr1 = ptr1->ptr2
-    //        { 
-    //            ptr2 = ptr1->sucessor;
-    //            delete ptr1;
-    //        }
-    //    }
-    //}
+        if (size_ != 0)
+        {
+            Object *ptr_other_iterator = other.head_;
+            Object *ptr_this = new Object(other.head_->data_);
+            head_ = ptr_this; // after: head_->sucessor_ = ptr_this->sucessor_ (=nullptr);  
+                              //        head_->data_ = ptr_this->data_ (=other.head_->data_);
+            Object *ptr_other_data_copy = nullptr;
+
+            ptr_other_iterator = ptr_other_iterator->sucessor_;
+            while (ptr_other_iterator != nullptr)
+            {
+                ptr_other_data_copy = new Object(ptr_other_iterator->data_);
+                ptr_this->sucessor_ = ptr_other_data_copy;
+                ptr_this = ptr_other_data_copy; // РљРђРљ Р­РўРћ РџР РћРРЎРҐРћР”РРў, Р•РЎР›Р РЈ РњР•РќРЇ РќР• РџР•Р Р•Р“Р РЈР–Р•Рќ РћРџР•Р РђРўРћР = Р”Р›РЇ РѕР±СЉРµРєС‚РѕРІ Object
+                ptr_other_iterator = ptr_other_iterator->sucessor_;
+            }
+
+            
+        }
+    } 
+
+    // destructor.
+    ~LinkedList()
+    {
+        forceObjectdelete(head_);
+    }
+
+    void forceObjectDelete(Object *object)
+    {
+    
+    }
+    
+
+    size_t size() const 
+    {
+        return size_;
+    }
+
+    void clear()
+    {
+        while
+        {
+
+        }
+        size_ = 0;
+    }
 protected:
-    Object<Type> *head_;
-    Object<Type> *tail_;
-    int amount_;
+    Object *head_;
+    size_t  size_;
 };
 
 
@@ -224,15 +273,13 @@ protected:
 
 int main()
 {
-    // Задание 6.0: Проверка общей работоспособности системы.
+    // Р—Р°РґР°РЅРёРµ 6.0: РџСЂРѕРІРµСЂРєР° РѕР±С‰РµР№ СЂР°Р±РѕС‚РѕСЃРїРѕСЃРѕР±РЅРѕСЃС‚Рё СЃРёСЃС‚РµРјС‹.
     //////////////////////////////////////////////////////////////////////////////
-    InformationProgrammer Rustem(1, "c++", "Rustem", "Sirazetdinov",
+    Programmer Rustem1(1, "c++", "Rustem", "Sirazetdinov",
         "avesirazetdinov@gmail.com", "I_getsuga");
 
-    InformationProgrammer* ptr1 = &Rustem;
-    InformationProgrammer* ptr2 = new InformationProgrammer(*ptr1);
-    ptr2->SetLevel(8);
+    Programmer Rustem2 = Rustem1;
 
-    std::cout << *ptr1 << *ptr2;
-    delete ptr2;
+    std::cout << Rustem1 << Rustem2;
+    //delete ptr2;
 }
