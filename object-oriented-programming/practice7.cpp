1aae24a87458f40fba1fe9bc4b026b2c95db21fd
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2022-05-29 14:16:09 +0300

    Create practice7.cpp

---
+++ object-oriented-programming/practice7.cpp
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <iostream>
+#include <stdarg.h>
+
+template<class Type> Type max1 (int params_amount = 1, ...)
+{
+	auto *ptr = &params_amount; std::cout << ">>> " << ptr << " : " << *ptr << typeid(*ptr).name() << '\n';
+	/*ptr += 2;
+	std::cout << ">>> " << ptr << " : " << *ptr << typeid(*ptr).name() << '\n';*/
+	Type value_max = *(++ptr);
+	// Type &;
+	// static_cast<Type>(++(++ptr));
+	
+
+	char is_next = 'n';
+	while (is_next == 'n')
+	{
+		std::cout << ptr << " : " << *ptr << " " << typeid(*ptr).name() << '\n';
+		ptr += 1;
+		std::cin >> is_next;
+	}
+
+
+	//for (int param_num = 2; param_num < params_amount; param_num++)
+	//{
+	//	iterator++;
+	//	value_max = (value_max < *iterator) ? *iterator : value_max;
+	//}
+
+
+	return value_max;
+}
+
+int main()
+{
+	std::cout << max1<int>(4, 1, 2, 3, 4) << '\n';
+	std::cout << max1<double>(4, (double) 1, (double) 2, (double) 3, (double) 4) << '\n';
+	std::cout << max1<double>(4, 1, 2, 3, 4) << '\n';
+}
