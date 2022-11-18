c80b83dd7911de454db256b667ac77220066067e
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2022-11-18 19:41:05 +0300

    Update graph.cpp

--- object-oriented-programming-2/graph/graph.cpp
+++ object-oriented-programming-2/graph/graph.cpp
@@ -3,103 +3,137 @@
 // алгоритм Прима
 // алгоритм Белмана-Форда
 // алгоритм Крускала 2 варианта 
-// TestGraph.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+
 #include <iostream>
-#include <iomanip>  
-#include <set>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <typeinfo>
-#include <fstream>
-#include <stack>
 #include <queue>
 
-using namespace std;
+#define nline '\n'
 
-class Edge {
+
+
+class edge {
 public:
-	int from, to, weight;
-	Edge(int from = -1, int to = -1, int weight = 0) : from(from), to(to), weight(weight) {}
-	Edge(const Edge &E)
-	{
-		from = E.from;
-		to = E.to;
-		weight = E.weight;
+
+	edge(int source = -1, int target = -1, int weight = 0) {
+		_source = source;
+		_target = target;
+		_weight = weight;
 	}
-	int operator<(Edge &E)
-	{
-		return (weight < E.weight);
+
+	edge (const typename edge &edge) {
+		_source = edge._source;
+		_target = edge._target;
+		_weight = edge._weight;
 	}
-	friend ostream &operator<<(ostream &s, Edge &e);
+
+
+
+	const int &get_source() const noexcept {
+		return _source;
+	}
+
+	const int &get_target() const noexcept {
+		return _target;
+	}
+
+	const int &get_weight() const noexcept {
+		return _weight;
+	}
+
+
+
+
+	bool operator < (const edge &edge) const noexcept {
+		return (_weight < edge._weight);
+	}
+
+
+
+	friend std::ostream &operator << (std::ostream &output, const edge &edge);
+
+private:
+	int _source, _target;
+	int _weight;
 };
 
-ostream &operator<<(ostream &s, Edge &e)
+std::ostream &operator << (std::ostream &output, edge &edge)
 {
-	s << "From: " << e.from << ", to: " << e.to << ", weight: " << e.weight;
-	return s;
+	output << nline << "From: " << edge.get_source() << ", to: " << edge.get_target() 
+		<< ", weight: " << edge.get_weight();
+	
+	
+	return output;
 }
 
 int main()
 {
 
-	vector<vector<int> > mat =
+	std::vector<std::vector<int> > mat =
 	{
-	{0, 3, 1, 3, 0, 0, 0},
-	{3, 0, 0, 3, 0, 0, 0},
-	{1, 0, 0, 1, 0, 0, 0},
-	{3, 3, 1, 0, 1, 0, 0},
-	{0, 0, 0, 1, 0, 1, 2},
-	{0, 0, 0, 0, 1, 0, 3},
-	{0, 0, 0, 0, 2, 3, 0}
-	};
+		{0, 3, 1, 3, 0, 0, 0},
+		{3, 0, 0, 3, 0, 0, 0},
+		{1, 0, 0, 1, 0, 0, 0},
+		{3, 3, 1, 0, 1, 0, 0},
+		{0, 0, 0, 1, 0, 1, 2},
+		{0, 0, 0, 0, 1, 0, 3},
+		{0, 0, 0, 0, 2, 3, 0}
+	                            };
 
-	//used[i] = 0 вершина еще не появлялась при обработке
-	vector<int> used(7, 0);
-	// vector<int> dist(7, -1);
-	vector<int> dist(7, 10000000); // inf
+	std::vector<int> used(7, 0);
+	
+	std::vector<int> distance(7, std::numeric_limits<int>::max()); // case_1
+	// or 
+	// std::vector<int> distance(7, std::numeric_limits<double>::infinity());
+	// or 
+	// std::vector<int> distance(7, -1); // case_2
 
-	//BFS
-	queue<int> q;
-	q.push(0);
-	dist[0] = 0;
-	int step = 0;
 
-	while ( !q.empty() ) {
+	std::queue<int> queue_edges;
+	
+	queue_edges.push(0);
+	distance[0] = 0;
+    static int step = 0;
+
+	while ( !queue_edges.empty() ) {
 		
-		int vertex = q.front();
-		q.pop();
+		int vertex = queue_edges.front();
+		queue_edges.pop();
+		
+		// case_1
+		for ( int i = 6; i >= 0; i-- ) {
+			
+			if ( mat[vertex][i] > 0 && ( distance[vertex] + mat[vertex][i] < distance[i] )  ) {
+				distance[i] = distance[vertex] + mat[vertex][i];
+				queue_edges.push(i);
+			}
 
-		// case: -1
+		}
+		
+		std::cout << nline << "steps: " << step << " dist.: ";
+		
+		for ( const auto &element : distance ) {
+			std::cout << " " << element;
+		}
+		
+
+		// TODO: переписать
+	    // case_2 ЭТОТ СЛУЧАЙ НЕ БЫЛ РАЗОБРАН 
 		//for ( int i = 0; i < 7; i++ ) {
 		//	//есть ребро vertex->i, в i не заходили
 		//	if ( mat[vertex][i] > 0 && used[i] < 2 ) {
-		//		q.push(i);
+		//		queue_edges.push(i);
 		//		//вершину видели, но не обработали
 		//		used[i] = 1;
 
-		//		if ( dist[i] < 0 ) dist[i] = dist[vertex] + mat[vertex][i];
+		//		if ( distance[i] < 0 ) distance[i] = distance[vertex] + mat[vertex][i];
 		//		else {
-		//			if ( dist[vertex] + mat[vertex][i] < dist[i] )
-		//				dist[i] = dist[vertex] + mat[vertex][i];
+		//			if ( distance[vertex] + mat[vertex][i] < distance[i] )
+		//				distance[i] = distance[vertex] + mat[vertex][i];
 		//		}
 		//	}
 		//}
-		
-		// case: inf
-		for ( int i = 6; i >= 0; i-- ) {
-			//есть ребро vertex->i, в i не заходили
-			if ( mat[vertex][i] > 0 && dist[vertex] + mat[vertex][i] < dist[i] ) {
-				dist[i] = dist[vertex] + mat[vertex][i];
-				q.push(i);
-			}
-		}
-		std::cout << '\n' << "steps: " << step << " dist: ";
-		for ( int i = 0; i < 7; i++ ) {
-			std::cout << " " << dist[i];
-		}
-		//обработка вершины закончена
+
 
 		step++;
 	}
@@ -113,48 +147,53 @@ int main()
 		{4, 0, 0, 3, 0, 0, 0},
 		{5, 2, 0, 0, 0, 0, 5},
 		{0, 0, 0, 0, 0, 5, 0}
-	};
+	                            };
 	int mst_weight = 0;
 
 
-	std::cout << "\nMinimum spanning tree weight: " << mst_weight << endl;
+	std::cout << nline << "Minimum spanning tree weight: " << mst_weight << nline;
 
-	mst_weight = 0;     //Текущий вес остова.
-	//vector <int> used(7, 0); //использованные вершины
+	mst_weight = 0;     // Текущий вес остова.
+	
+	//std::vector <int> used(7, 0); //использованные вершины
 	//for ( int i = 0; i < 7; i++ ) used[i] = 0;
-	vector<Edge> edges;		//рассматриваемые ребра
-	vector<Edge> tree_edges;	//ребра в минимальном остове
+	
+	std::vector<edge> edges;		// рассматриваемые ребра
+	std::vector<edge> tree_edges;	// ребра в минимальном остове
 
-	edges.push_back(Edge(0, 0, 0));     //Начнём с вершины 0. // фиктивная вершина
+	edges.push_back(edge(0, 0, 0));     // Начнём с вершины 0. // фиктивная вершина
 
 
 	while ( !edges.empty() ) {
-		auto it = min_element(edges.begin(), edges.end());
-		int to = it->to;
-		int from = it->from;
-		int w = it->weight;
-		mst_weight += w;
-		edges.erase(it);
-
-		if ( used[to] ) continue;
-		used[to] = 1;
-
-		tree_edges.push_back(Edge(from, to, w));
 		
-		for ( int i = 0; i < 7 ; i++ ) {
-			if ( mat[to][i] > 0 && used[i] ) {
-				edges.push_back(Edge(to, i, mat[to][i]));
+		auto iter = std::min_element(edges.begin(), edges.end());
+		
+		int target = iter->get_target();
+		int source = iter->get_source();
+		int weight = iter->get_weight();
+		
+		mst_weight += weight;
+		edges.erase(iter);
+
+		if ( used[target] ) { continue; }
+		used[target] = 1;
+
+		tree_edges.push_back(edge(source, target, weight));
+		
+		for ( int source_current = 0; source_current < 7 ; source_current++ ) {
+			if ( mat[target][source_current] > 0 && used[source_current] ) {
+				edges.push_back(edge(target, source_current, mat[target][source_current]));
 			}
 		}
 	}
 
-	std::cout << "\nMinimum spanning tree weight: " << mst_weight << endl;
+	std::cout << nline << "Minimum spanning tree weight: " << mst_weight << nline;
+	
 	for ( int i = 1; i < tree_edges.size(); i++ ) {
-		cout << "\nEdge " << i << " ";
-		cout << tree_edges[i];
+		std::cout << nline << "Edge " << i << " " << tree_edges[i];
 	}
 
-	//char c; cin >> c;
+
 
 	return 0;
 }
