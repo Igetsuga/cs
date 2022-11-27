f66b3757ba162d668aad734f887a832d5172128f
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2022-11-27 16:05:01 +0300

    before refactor find_key & find_value

--- object-oriented-programming-2/pair/pair.cpp
+++ object-oriented-programming-2/pair/pair.cpp
@@ -15,133 +15,138 @@ namespace ds_lib {
 // ---------------------------------------------------------------------------------------------
 
 
-	template <typename Type1, typename Type2> class ipair {
-	public:
+template <typename Type1, typename Type2> class ipair {
+public:
 
-		virtual Type1 first () const noexcept = 0;
-		virtual Type2 second () const noexcept = 0;
+	virtual Type1 first () const noexcept = 0;
+	virtual Type2 second () const noexcept = 0;
 
-	};
+};
 
 // ---------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------
 
 
-	template<typename Type1, typename Type2> class pair : ipair<Type1, Type2> {
-	public:
+template<typename Type1, typename Type2> class pair : ipair<Type1, Type2> {
+public:
 
-		pair (const Type1 &value1, const Type2 &value2) {
-			_value1 = value1;
-			_value2 = value2;
-		}
-
-		pair (const pair &pair_other) {
-			_value1 = pair_other._value1;
-			_value2 = pair_other._value2;
-		}
-
-
-
-		virtual ~pair() = default;
-
-
-
-		virtual Type1 first ()  const noexcept override final { return _value1; }
-		virtual Type2 second () const noexcept override final { return _value2; }
-
-
-
-		virtual bool operator == (const pair &pair_other) const noexcept  {
-			return (_value1 == pair_other._value1 && _value2 == pair_other._value2 ? true : false);
-		}
-
-		virtual bool operator > (const pair &pair_other) const noexcept {
-			if ( _value1 > pair_other._value1 ) {
-				return true;
-			}
-			else if ( _value1 == pair_other._value1 ) {
-				return (_value2 > pair_other._value2 ? true : false);
-			}
-
-
-			return false;
-		}
-
-		bool operator < (const pair &pair_other) const noexcept  {
-			return !(*this > pair_other);
-		}
-
-		bool operator >= (const pair &pair_other) const noexcept  {
-			return (this > pair_other || this == pair_other);
-		}
-
-		bool operator <= (const pair &pair_other) const noexcept  {
-			return (this < pair_other || this == pair_other);
-		}
-
-
-
-	protected:
-		Type1 _value1;
-		Type2 _value2;
-	};
-
-	template<typename Type1, typename Type2>
-	std::ostream &operator<< (std::ostream &output, const pair<Type1, Type2> &pair) {
-		// output << '\n' << "first: " << pair.first() << '\n' << "second: " << pair.second() << '\n';
-		// output << '\n' << "{first, second} : {" << pair.first() << ", " << pair.second() << "}; " << '\n';
-		output << "(" << pair.first() << ", " << pair.second() << ")";
-
-		return output;
+	pair (const Type1 &value1, const Type2 &value2) {
+		_value1 = value1;
+		_value2 = value2;
 	}
 
 
-	template<typename Type1, typename Type2>
-	ds_lib::pair<Type1, Type2> &make_pair (const Type1 &value1, const Type2 &value2) noexcept {
-		return ds_lib::pair<Type1,Type2>(value1, value2);
+	pair (const pair &pair_other) {
+		_value1 = pair_other._value1;
+		_value2 = pair_other._value2;
 	}
 
-	template<typename Type1, typename Type2>
-	std::pair<Type1, Type2> &make_std_pair (const Type1 &value1, const Type2 &value2) noexcept {
-		return std::make_pair(value1, value2);
+
+
+	virtual ~pair() = default;
+
+
+
+	virtual Type1 first ()  const noexcept override final { return _value1; }
+	virtual Type2 second () const noexcept override final { return _value2; }
+
+
+
+	virtual bool operator == (const pair &pair_other) const noexcept {
+		return (_value1 == pair_other._value1 && _value2 == pair_other._value2 ? true : false);
 	}
 
-// ---------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------
-
-
-	class bike_key final : public ds_lib::pair<std::string, int> {
-	public:
-
-		bike_key (const std::string &brand, int diameter) : pair (brand, diameter) {};
-
-
-		bool operator > (const pair &pair_other) const noexcept override {
-			if ( _value2 > pair_other.second() ) {
-				return true;
-			}
-
-
-			return false;
+	virtual bool operator > (const pair &pair_other) const noexcept {
+		if ( _value1 > pair_other._value1 ) {
+			return true;
+		}
+		else if ( _value1 == pair_other._value1 ) {
+			return (_value2 > pair_other._value2 ? true : false);
 		}
 
 
-		virtual ~bike_key() = default;
+		return false;
+	}
 
-	};
+	bool operator < (const pair &pair_other) const noexcept {
+		return !(*this > pair_other);
+	}
+
+	bool operator >= (const pair &pair_other) const noexcept {
+		return (this > pair_other || this == pair_other);
+	}
+
+	bool operator <= (const pair &pair_other) const noexcept {
+		return (this < pair_other || this == pair_other);
+	}
+
+
+
+protected:
+	Type1 _value1;
+	Type2 _value2;
+};
+
+template<typename Type1, typename Type2>
+std::ostream &operator<< (std::ostream &output, const pair<Type1, Type2> &pair) {
+	// output << '\n' << "first: " << pair.first() << '\n' << "second: " << pair.second() << '\n';
+	// output << '\n' << "{first, second} : {" << pair.first() << ", " << pair.second() << "}; " << '\n';
+	output << "(" << pair.first() << ", " << pair.second() << ")";
+
+	return output;
+}
+
+
+template<typename Type1, typename Type2>
+ds_lib::pair<Type1, Type2> &make_pair (const Type1 &value1, const Type2 &value2) noexcept {
+	return ds_lib::pair<Type1, Type2>(value1, value2);
+}
+
+template<typename Type1, typename Type2>
+std::pair<Type1, Type2> &make_std_pair (const Type1 &value1, const Type2 &value2) noexcept {
+	return std::make_pair(value1, value2);
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+
+class bike_key final : public ds_lib::pair<std::string, int> {
+public:
+
+	bike_key (const std::string &brand = "", int diameter = -1) : pair (brand, diameter) {};
+
+	//bike_key (const bike_key &key_other) {
+	//	_value1 = key_other._value1;
+	//	_value2 = key_other._value2;
+	//}
+
+	bool operator > (const pair &pair_other) const noexcept override {
+		if ( _value2 > pair_other.second() ) {
+			return true;
+		}
+		
+
+		return false;
+	}
+
+
+	virtual ~bike_key() = default;
+
+};
 
 
 // ---------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------
 
 
-	class bike_value final : public ds_lib::pair<std::string, bool> {
-	public:
+class bike_value final : public ds_lib::pair<std::string, bool> {
+public:
 
-		bike_value (const std::string &type, bool is_absorb) : pair (type, is_absorb) {};
+	bike_value (const std::string &type, bool is_absorb) : pair (type, is_absorb) {};
 
-		virtual ~bike_value() = default;
-	};
+	virtual ~bike_value() = default;
+};
 
 
 // ---------------------------------------------------------------------------------------------
@@ -162,7 +167,7 @@ namespace ds_lib {
 ds_lib::bike_value find_value (const std::map<ds_lib::bike_key, ds_lib::bike_value> &map,
 							   const ds_lib::bike_key &key) noexcept {
 	ds_lib::bike_value answer("NONE", 0);
-	
+
 	for ( const auto &element : map ) {
 		if ( element.first == key ) {
 			answer = element.second;
@@ -177,7 +182,7 @@ ds_lib::bike_value find_value (const std::map<ds_lib::bike_key, ds_lib::bike_val
 const ds_lib::bike_key find_key (const std::map<ds_lib::bike_key, ds_lib::bike_value> &map,
 								 const ds_lib::bike_value &value) noexcept {
 	ds_lib::bike_key answer("NONE", -1);
-	
+
 	for ( const auto &element : map ) {
 		if ( element.second == value ) {
 			answer = element.first;
@@ -221,9 +226,9 @@ template<typename Type1, typename Type2> void print_iter (const std::map<Type1, 
 
 template<typename Iterator_type>
 void print_iter (Iterator_type begin, Iterator_type end) {
-	
+
 	std::cout << "{" << '\n';
-	
+
 	while ( begin != end ) {
 		std::cout << (*begin).first << " --- " << (*begin).second << '\n';
 		begin++;
@@ -240,16 +245,16 @@ bool func_key (const ds_lib::bike_value &value, const int &threshold = 0) {
 	return (value.second() > threshold);
 }
 
-using func = bool(*)(const ds_lib::bike_value&, const int&);
+using func = bool(*)(const ds_lib::bike_value &, const int &);
 func func_task4 = &func_key;
 
 // TODO: doesn't work with &filter, idk
-template<typename Type1, typename Type2> 
+template<typename Type1, typename Type2>
 std::map<Type1, Type2> filter (const std::map<Type1, Type2> &map,
-								bool(*func_key)(const ds_lib::bike_value&, const int &),
-								const int &threshold) {
+							   bool(*func_key)(const ds_lib::bike_value &, const int &),
+							   const int &threshold) {
 	std::map<Type1, Type2> map_result;
-	
+
 	for ( const auto &element : map ) {
 		if ( func_key(element.second, threshold) == true ) {
 			//std::cout << typeid(element).name();
@@ -272,8 +277,7 @@ std::map<Type1, Type2> filter (const std::map<Type1, Type2> &map,
 // --------------------------------------------task5--------------------------------------------
 // ---------------------------------------------------------------------------------------------
 
-class Exception : public std::exception
-{
+class Exception : public std::exception {
 public:
 
 	Exception()
@@ -282,7 +286,7 @@ public:
 	}
 
 
-	Exception(const std::string &message)
+	Exception (const std::string &message)
 	{
 
 		message_ = new std::string(message);
@@ -291,7 +295,7 @@ public:
 	}
 
 	// constructor_copy
-	Exception(const Exception &exception)  
+	Exception (const Exception &exception)
 	{
 		message_ = new std::string(*exception.message_);
 		// message_ = new char[strlen(exception.message_) + 1];
@@ -301,32 +305,70 @@ public:
 	// destructor
 	virtual ~Exception()
 	{
-		delete[] message_; message_ = nullptr;
+		delete [] message_; message_ = nullptr;
 	}
 
 	virtual void what()
 	{
-		std::cout  << '\n' << "{Exception}: " << message_ << "; " << std::exception::what();
+		std::cout << '\n' << "{Exception}: " << message_ << "; " << std::exception::what();
 	}
+
+protected:
+	std::string *message_; // error message
+};
+
+class invalid_key : public Exception {
+public:
 	
-	protected:
-	std::string* message_; // error message
+	invalid_key (const std::string &message, ds_lib::bike_key &key) : Exception(message) {
+		key_ = key;
+	}
+
+	virtual ~invalid_key() = default;
+
+	virtual void what() override {
+		std::cout << '\n' << "{invalid_value}: " << message_ << "; " << std::exception::what();
+	}
+
+protected:
+	ds_lib::bike_key key_;
+};
+
+class repeated_key : public invalid_key {
+public:
+
+	repeated_key (const std::string &message, ds_lib::bike_key &key,
+				  std::map<ds_lib::bike_key, ds_lib::bike_value> &map) : invalid_key(message, key) {
+		map_ = map;
+	}
+
+	virtual ~repeated_key() = default;
+
+	virtual void what() override {
+		std::cout << '\n' << "{repeated_key}: " << message_ << "; " << std::exception::what();
+	}
+
+protected:
+	std::map<ds_lib::bike_key, ds_lib::bike_value> map_;
 };
 
 
 
 
 
-
-
-
-//std::map<ds_lib::bike_key, ds_lib::bike_value> &insert(std::map<ds_lib::bike_key, ds_lib::bike_value> map,
-//													   std::pair<ds_lib::bike_key, ds_lib::bike_value> pait) {
-
-//}
+template <typename Key, typename Value>
+std::pair<typename std::map<Key, Value>::iteraror, bool> &insert(std::map<Key, Value> &map,
+														         const std::pair<ds_lib::bike_key, ds_lib::bike_value> &pair) {
+	if ( find_key(map, pair.second) != map.end() ) {
+		throw repeated_key("invalid_kye: key already exists in container", pair.first, map);
+	}
+	else {
+		return map.insert(pair);
+	}
+}
 
 // ---------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------
+// --------------------------- ------------------------------------------------------------------
 
 using namespace ds_lib;
 int main() {
@@ -344,7 +386,7 @@ int main() {
 	for ( int i = 0; i < 13; i++ ) {
 		vector_key.push_back(bike_key(key + std::to_string(i), std::rand()));
 		vector_value.push_back(bike_value(value + std::to_string(i), std::rand() % 2));
- 
+
 		bike_map.insert({vector_key[i], vector_value[i]});
 	}
 	// Проверка правильного постороения(правилность сортировки по ключу) структуры std::map
@@ -363,7 +405,7 @@ int main() {
 	auto find_value_key10 = find_value(bike_map, vector_key[10]);
 	std::cout << "RESULT VALUE: " << find_value_key1 << " --> TRUE VALUE: " << vector_value[1] << '\n';
 	std::cout << "RESULT VALUE: " << find_value_key10 << " --> TRUE VALUE: " << vector_value[10] << '\n';
-	
+
 	// Поиск по значению
 	auto find_key_value1 = find_key(bike_map, vector_value[1]);
 	auto find_key_value11 = find_key(bike_map, vector_value[11]);
@@ -381,7 +423,7 @@ int main() {
 // --------------------------------------------task3--------------------------------------------
 // ---------------------------------------------------------------------------------------------
 
-    // Вывод через итераторы
+	// Вывод через итераторы
 	print_iter(bike_map);
 
 // ---------------------------------------------------------------------------------------------
@@ -395,17 +437,17 @@ int main() {
 	//std::map<ds_lib::bike_key, ds_lib::bike_value> map_train;
 	//map_train.insert({vector_key[0], vector_value[0]});
 	//map_train.insert({vector_key[1], vector_value[1]});
-	
+
 
 	map_result = filter(bike_map, func_task4, 0);
-	print_iter( map_result.begin(), map_result.end() );
+	print_iter(map_result.begin(), map_result.end());
 
 // ---------------------------------------------------------------------------------------------
 // --------------------------------------------task5--------------------------------------------
 // ---------------------------------------------------------------------------------------------
 
-
-
+	auto pair_new = std::make_pair(ds_lib::bike_key("key_new", 999), ds_lib::bike_value("value_new", 1));
+	std::cout << pair_new.first << pair_new.second;
 
 
 	return 0;
